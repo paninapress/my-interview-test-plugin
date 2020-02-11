5511fecc19b7a997d2195851d6f3e2364ab266a0
paninapress <npacifico25@gmail.com>
2020-02-11 08:43:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+My interview test plugin
+=====================
+
---
+++ ninas-interview-test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Nina's Interview Test Plugin
+ * Author: 		Nina Pacifico
+ * Description: Creating this plugin from scratch as a Sandhills Development Interview Test
+ * Version:     1.0.0
+ */
+
+/*********************************
+* includes
+*********************************/
+
+?>
